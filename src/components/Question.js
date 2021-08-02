9804af6b1949a1b751ddeda077c36c2f0b160d58
Niyaz <niyaz.irfan119@gmail.com>
2021-08-02 20:41:47 +0300

    commit tree c115f5f41d15fcad6e7dc972be4ab40d2685ec05

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(()=>{
+    if(timeRemaining===0 ){
+      setTimeRemaining(10);
+      onAnswered(false)
+    }
+   
+
+    
+const time=setTimeout(()=>{
+    setTimeRemaining(prev=>prev-1);
+
+  
+},1000)
+    
+return function cleanup() {
+  clearTimeout(time);
+}
+
+
+  },);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
